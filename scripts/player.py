d01ebfbf88f1aba4f8e4c710328571d10cc7d2c6
NadineMarques <nady_markes@hotmail.com>
2024-02-11 16:55:35 -0300

    Adds snake tails

--- scripts/player.py
+++ scripts/player.py
@@ -12,6 +12,7 @@ class Player(GameObject):
         self.direction = "right"
         self.running = True
         self.apple = self.get_apple_object()
+        self.tails = 0
         threading.Thread(target=self.move).start()
 
     def get_apple_object(self) -> Apple:
@@ -47,11 +48,12 @@ class Player(GameObject):
             if self.pos[1] < 0:
                 self.pos[1] = config.BLOCKS_PER_LINE - 1
 
-            pygame.time.wait(500)
+            pygame.time.wait(200)
 
     def get_apple_collision(self):
         if self.pos[0] == self.apple.pos[0] and self.pos[1] == self.apple.pos[1]:
             self.apple.respawn()
+            self.tails += 1
 
     def process(self) -> None:
         pygame.draw.rect(self.screen, 'green', self.get_position() + [config.PLAYER_SIZE, config.PLAYER_SIZE])
@@ -66,3 +68,16 @@ class Player(GameObject):
             self.direction = "left"
         if keys[pygame.K_d] and self.direction != "left":
             self.direction = "right"
+        
+        for i in range(0, self.tails):
+            self.grow(i)
+
+    def grow(self, index) -> None:
+        if self.direction == "up":
+            pygame.draw.rect(self.screen, 'green', [self.pos[0] * config.PLAYER_SIZE, (self.pos[1]  + index + 1) * config.PLAYER_SIZE] + [config.PLAYER_SIZE, config.PLAYER_SIZE], int(config.PLAYER_SIZE), 10)
+        if self.direction == "down":
+            pygame.draw.rect(self.screen, 'green', [self.pos[0] * config.PLAYER_SIZE, (self.pos[1] - index - 1) * config.PLAYER_SIZE] + [config.PLAYER_SIZE, config.PLAYER_SIZE], int(config.PLAYER_SIZE), 10)
+        if self.direction == "left":
+            pygame.draw.rect(self.screen, 'green', [(self.pos[0] + index + 1) * config.PLAYER_SIZE, self.pos[1] * config.PLAYER_SIZE] + [config.PLAYER_SIZE, config.PLAYER_SIZE], int(config.PLAYER_SIZE), 10)
+        if self.direction == "right":
+            pygame.draw.rect(self.screen, 'green', [(self.pos[0] - index - 1) * config.PLAYER_SIZE, self.pos[1] * config.PLAYER_SIZE] + [config.PLAYER_SIZE, config.PLAYER_SIZE], int(config.PLAYER_SIZE), 10)
